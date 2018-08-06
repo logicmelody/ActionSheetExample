fee83c1bf96d779abbf9585c6c991aa5cbed2495
Danny Lin <danny.l@ljit.io>
2018-08-06 17:18:01 +0800

    Implement action sheet

--- src/app.js
+++ src/app.js
@@ -1,15 +1,69 @@
 import React, { Component } from "react";
 import {
+	StyleSheet,
 	Text,
 	View,
+	Button,
 } from "react-native";
+
+import Modal from "react-native-modalbox";
 
 class App extends Component {
 	render() {
 		return (
-			<Text>App</Text>
+			<View style={styles.container}>
+				<Button
+					style={styles.btn}
+					onPress={() => this.refs.myModal.open()}
+					title={"My Modal"} />
+
+				<Modal
+					style={styles.myModal}
+					position={"bottom"}
+					ref={"myModal"}
+				>
+					<View style={styles.shoppingCartContainer}>
+						<Text style={styles.text}>Shopping Cart</Text>
+					</View>
+				</Modal>
+			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	// 使用react-native-modalbox的時候，最外層的View的style要將flex設為1
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+
+	// 最外層的Modal來設定Modal的height
+	myModal: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 500,
+	},
+
+	// Modal中的View，記得要設定flex: 1跟alignSelf: "stretch"，讓View撐滿整個Modal，
+	// 不然點擊Modal以外的backdrop讓Modal消失這個功能會有問題
+	shoppingCartContainer: {
+		flex: 1,
+		alignSelf: "stretch",
+		backgroundColor: "blue",
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		color: "white",
+		fontSize: 22
+	},
+	btn: {
+		backgroundColor: "#3B5998",
+		color: "white",
+		padding: 10,
+	},
+});
 
 export default App;
